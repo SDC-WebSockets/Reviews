f4187c619ac8261e779dca15b0d1f80ec50a8bba
Frederic Rosselet <fred.rosselet@gmail.com>
2021-04-26 08:23:19 -0700

    added more dependencies and a working server

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-react-jsx']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/public'
+  },
+
+  mode: 'development',
+  watch: true
+};
