4af9c23c3fb0ae7fb114f2b41f2e49bcb144e1bb
Frederic Rosselet <fred.rosselet@gmail.com>
2021-04-26 20:00:48 -0700

    added 100 empty ratings

--- database/mongoDb.js
+++ database/mongoDb.js
@@ -7,20 +7,8 @@ mongoDb.once('open', () => {
   console.log('Connected to the MongoDB database');
 });
 
-const ratingSchema = new mongoose.Schema({ // 1 -> 1 relationship with course_id
-  course_id: Number,
-  overallRating: Number,
-  total: Number,
-  fiveStars: Number,
-  fourStars: Number,
-  threeStars: Number,
-  twoStars: Number,
-  oneStar: Number
-});
-
-const reviewSchema = new mongoose.Schema({ // 1 -> many relationship with course_id
-  course_id: Number,
-  reviewId: Number,
+const reviewSchema = new mongoose.Schema({ // 1 -> many: course_id -> reviews
+  course_id: Number,  // "foreign key"
   reviewer: Object,   // {“reviewerId”: Number,
                       // “name”: String,
                       // “pictureUrl”: String,
@@ -33,43 +21,124 @@ const reviewSchema = new mongoose.Schema({ // 1 -> many relationship with course
   reported: Boolean
 });
 
+const ratingSchema = new mongoose.Schema({ // 1 <-> 1: course_id <-> rating
+  course_id: Number,
+  overallRating: Number,
+  total: Number,
+  fiveStars: Number,
+  fourStars: Number,
+  threeStars: Number,
+  twoStars: Number,
+  oneStar: Number
+});
+
+let Review = mongoose.model('Review', reviewSchema);
 let Rating = mongoose.model('Rating', ratingSchema);
 
-const saveRating = (rating) => {
+const addReview = (review) => {
+  Review.updateOne({ }, {
+        course_id: review.course_id,  // "foreign key"
+        reviewer: {
+          name: review.reviewer.name,
+          picture: review.reviewer.picture,
+          coursesTaken: review.reviewer.coursesTaken,
+          reviews: review.reviewer.reviews
+        },
+        rating: review.rating,
+        comment: review.comment,
+        createdAt: review.createdAt,
+        helpful: 0,
+        reported: false
+      }
+    , {upsert:true}, (err) => {
+      if (err) {
+        console.log('Error saving review to database:', err);
+      } else {
+        console.log(`Review for course id ${review.course_id} saved/updated in database`);
+        let filter = {course_id: review.course_id};
+        let options = {new: true, useFindAndModify: false};
+        let callback = (err) => {
+          err ? console.log('Error updating rating in database:', err) : console.log(`Updated rating for course id ${review.course_id}`);
+        };
+        if (review.rating === 5) {
+          Rating.findOneAndUpdate(filter, { $inc: {fiveStars: 1}}, options, callback);
+        } else if (review.rating === 4) {
+          Rating.findOneAndUpdate(filter, { $inc: {fourStars: 1}}, options, callback);
+        } else if (review.rating === 3) {
+          Rating.findOneAndUpdate(filter, { $inc: {threeStars: 1}}, options, callback);
+        } else if (review.rating === 2) {
+          Rating.findOneAndUpdate(filter, { $inc: {twoStars: 1}}, options, callback);
+        } else if (review.rating === 1) {
+          Rating.findOneAndUpdate(filter, { $inc: {oneStar: 1}}, options, callback);
+        }
+      }
+    })
+}
 
-  let totalRatings = rating.fiveStars + rating.fourStars + rating.threeStars + rating.twoStars + rating.oneStar;
-  let averageRating = (((rating.fiveStars * 5) + (rating.fourStars * 4) + (rating.threeStars * 3) + (rating.twoStars * 2) + rating.oneStar) / totalRatings).toFixed(1);
+let exampleReview = {
+  course_id: 1,  // "foreign key"
+  reviewer: {
+    name: 'Fred Rosselet',
+    picture: 'someUrl',
+    coursesTaken: 12,
+    reviews: 10
+  },
+  rating: 5,
+  comment: 'awesome course. Highly recommend.',
+  createdAt: Date.now(),
+  helpful: 0,
+  reported: false
+}
 
-  Rating.updateOne({course_id: rating.course_id},
-    {
-      course_id: rating.course_id,
-      overallRating: averageRating,
-      total: totalRatings,
-      fiveStars: rating.fiveStars,
-      fourStars: rating.fourStars,
-      threeStars: rating.threeStars,
-      twoStars: rating.twoStars,
-      oneStar: rating.oneStar
-    }
-  ,{upsert:true}, (err) => {
-    if (err) {
-      console.log('Error saving rating to database:', err);
-    } else {
-      console.log(`Rating for course id ${rating.course_id} saved/updated in database`);
-    }
-  })
-};
+// addReview(exampleReview);
 
 
+// ---------    DO NOT UNCOMMENT (used for populating ratings collection empty listings)
 
-// let ratingExample = { // total = 6; average = 4.2
-//   course_id: 1,
-//   fiveStars: 4,
-//   fourStars: 1,
-//   threeStars: 0,
-//   twoStars: 0,
-//   oneStar: 1
+// const saveRating = (rating) => {
+//   Rating.updateOne({course_id: rating.course_id},
+//     {
+//       course_id: rating.course_id,
+//       overallRating: 0,
+//       total: 0,
+//       fiveStars: 0,
+//       fourStars: 0,
+//       threeStars: 0,
+//       twoStars: 0,
+//       oneStar: 0
+//     }
+//   ,{upsert: true}, (err) => {
+//     if (err) {
+//       console.log('Error saving rating to database:', err);
+//     } else {
+//       console.log(`Rating for course id ${rating.course_id} saved/updated in database`);
+//     }
+//   })
+// };
+
+
+// // RESET ONE RATING
+// let exampleRating = {
+//   course_id: 1
 // }
 
-// saveRating(ratingExample);
+// saveRating(exampleRating);
+
+
+// // RESET 100 RATINGS
+// const resetRatings = () => {
+//   let exampleRating = {
+//     course_id: 1
+//   }
+//   let numberOfTimes = 100;
+//   let delay = 500;
+//   for (let i = 0; i < numberOfTimes; i++) {
+//     setTimeout(() => {
+//     saveRating(exampleRating);
+//       exampleRating.course_id++;
+//     }, delay * i);
+//   }
+// }
+
+// resetRatings();
 
