df04a16d47f61e51c23f7b0a56f9dca4edbd64ed
Frederic Rosselet <fred.rosselet@gmail.com>
2021-05-06 10:25:05 -0700

    implemented X button functionality next to each star tier in Feedback component

--- client/src/components/feedback.jsx
+++ client/src/components/feedback.jsx
@@ -1,40 +1,74 @@
 import React from 'react';
 
-const Feedback = (props) => {
-  // console.log('Props in Feedback:', props);
-  if (props.ratings.totalRatings === 0) {
-    return (
-      <div>
-        <h2>Student feedback</h2>
-        <div>This course does not have any ratings yet.</div>
-      </div>
-    );
-  } else {
-    const getPercentage = (tier1 = 0, tier2 = 0) => {
-      let percentage = (tier1 + tier2) / props.ratings.totalRatings * 100;
-      if (0 < percentage && percentage < 1) {
-        return '< 1%';
-      }
-      return Math.round(percentage) + '%';
+class Feedback extends React.Component {
+  constructor(props) {
+    // console.log('Props in Feedback:', props);
+    super(props);
+    this.removeFilter = this.removeFilter.bind(this);
+    this.state = {
+      tierClicked: null
     };
-    // if a percentage is 0%, render it gray
-    return (
-      <div>
-        <h2>Student feedback</h2>
-        <div>{props.ratings.overallRating.toFixed(1)} Course Rating</div> {/* average of all ratings */}
-        <div onClick={() => props.setReviewsFilteredByTier('5')}>
-          5 stars: {getPercentage(props.ratings['5'])}</div>
-        <div onClick={() => props.setReviewsFilteredByTier('4')}>
-          4 stars: {getPercentage(props.ratings['4 1/2'], props.ratings['4'])}</div>
-        <div onClick={() => props.setReviewsFilteredByTier('3')}>
-          3 stars: {getPercentage(props.ratings['3 1/2'], props.ratings['3'])}</div>
-        <div onClick={() => props.setReviewsFilteredByTier('2')}>
-          2 stars: {getPercentage(props.ratings['2 1/2'], props.ratings['2'])}</div>
-        <div onClick={() => props.setReviewsFilteredByTier('1')}>
-          1 star: {getPercentage(props.ratings['1 1/2'], props.ratings['1'])}</div>
-      </div>
-    );
   }
-};
+
+  getPercentage(tier1 = 0, tier2 = 0) {
+    let percentage = (tier1 + tier2) / this.props.ratings.totalRatings * 100;
+    if (0 < percentage && percentage < 1) {
+      return '< 1%';
+    }
+    return Math.round(percentage) + '%';
+  }
+
+  handleClick(tier) {
+    this.props.setReviewsFilteredByTier(tier);
+    this.setState({tierClicked: tier});
+  }
+
+  removeFilter() {
+    this.props.setReviewsFilteredByTier('all');
+    this.setState({tierClicked: null});
+  }
+
+  // if a percentage is 0%, render it gray
+  render() {
+    if (this.props.ratings.totalRatings === 0) {
+      return (
+        <div>
+          <h2>Student feedback</h2>
+          <div>This course does not have any ratings yet.</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>Student feedback</h2>
+          <div>{this.props.ratings.overallRating.toFixed(1)} Course Rating</div>
+
+          <div onClick={() => this.handleClick('5')}>
+            5 stars: {this.getPercentage(this.props.ratings['5'])}</div>
+          {this.state.tierClicked === '5' ? <button onClick={this.removeFilter}>X</button> : null}
+
+          <div onClick={() => this.handleClick('4')}>
+            4 stars: {this.getPercentage(this.props.ratings['4 1/2'], this.props.ratings['4'])}</div>
+          {this.state.tierClicked === '4' ? <button onClick={this.removeFilter}>X</button> : null}
+
+          <div onClick={() => this.handleClick('3')}>
+            3 stars: {this.getPercentage(this.props.ratings['3 1/2'], this.props.ratings['3'])}</div>
+          {this.state.tierClicked === '3' ? <button onClick={this.removeFilter}>X</button> : null}
+
+          <div onClick={() => this.handleClick('2')}>
+            2 stars: {this.getPercentage(this.props.ratings['2 1/2'], this.props.ratings['2'])}</div>
+          {this.state.tierClicked === '2' ? <button onClick={this.removeFilter}>X</button> : null}
+
+          <div onClick={() => this.handleClick('1')}>
+            1 star: {this.getPercentage(this.props.ratings['1 1/2'], this.props.ratings['1'])}</div>
+          {this.state.tierClicked === '1' ? <button onClick={this.removeFilter}>X</button> : null}
+
+        </div>
+      );
+    }
+  }
+}
+
+
 
 export default Feedback;
