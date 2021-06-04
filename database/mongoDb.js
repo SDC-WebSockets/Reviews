a09b5e4b4b434b217cbbc152962f1c35993e821f
Frederic Rosselet <fred.rosselet@gmail.com>
2021-06-04 14:14:46 -0700

    last env variable change

--- database/mongoDb.js
+++ database/mongoDb.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const host = process.env.HOST || 'localhost';
 
-mongoose.connect(`mongodb://${host}/review-service`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/review-service', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const mongoDb = mongoose.connection;
 mongoDb.on('error', console.error.bind(console, 'connection error:'));
 mongoDb.once('open', () => {
-  console.log(`Connected to the MongoDB database at ${host}`);
+  console.log('Connected to the MongoDB database');
 });
 
 const reviewSchema = new mongoose.Schema({ // 1 -> many: course_id -> reviews
