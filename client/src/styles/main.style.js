70a4be2a27958a164a21f77d8561dccf408ceddb
Frederic Rosselet <fred.rosselet@gmail.com>
2021-06-05 12:22:23 -0700

    last styling change

--- client/src/styles/main.style.js
+++ client/src/styles/main.style.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const ReviewServiceWrapper = styled.div`
-  /* box-sizing: border-box; */
+  box-sizing: border-box;
   color: rgb(60, 59, 55);
   display: block;
   font-family: "sf pro text", "-apple-system", system-ui, "Roboto", "segoe ui", Helvetica, Arial, sans-serif, "apple color emoji", "segoe ui emoji", "segoe ui symbol";
   font-size: 14px;
   font-weight: 400;
-  /* float: left; */
+  float: left;
   letter-spacing: -0.2px
   line-height: 22.4px;
   /* margin: auto; */
